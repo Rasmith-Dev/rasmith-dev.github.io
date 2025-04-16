037f20b1fa2a00e8deb975b6fd26912d87fffb7f
Rishi <134858974+Rasmith-Dev@users.noreply.github.com>
2025-04-16 17:41:43 +0530

    Update personal-information.component.ts
    updated

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -9,16 +9,16 @@ export class PersonalInformationComponent implements OnInit {
   myData: string[][] = [
     ['Name', 'Arasada Rasmith Patnaik'],
     ['DOB', '16/03/1998'],
-    ['Work Exp', '2.7 Years'],
+    ['Work Exp', '4.6 Years'],
     ['Education', 'B.Tech (2020)'],
-    ['Interests', 'Writing'],
+    ['Interests', 'Developing and Deploying Solutions'],
   ];
 
   aboutMe: string[] = [
-    'Hi, I am a Java Full Stack Developer with more than 2.6 years of experience in software industry.',
+    'Hi, I am a Java Full Stack Developer with more than 4.6 years of experience in software industry.',
     'Day-to-Day involves creating modular and scalable microservice systems. Responsible for Developing code for software components and applications',
     'Delivered all Projects withing deadlines. Always eager to learn new technologies.',
-    'Currently, working as Senior Project Engineer at Wipro',
+    'Currently, working as Application Developer at Thoughtworks',
   ];
 
   constructor() {}
